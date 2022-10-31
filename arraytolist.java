5b387236de6a4e2d2d46d4b885bbac41e7a4b017
kamal00007 <117075570+kamal00007@users.noreply.github.com>
2022-10-31 14:59:05 +0530

    array to list java program

---
+++ arraytolist.java
@@ -0,0 +1,30 @@
+package arrrayutil;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ArraytoList {
+
+	public static void main(String[] args) {
+		String[] s= {"Z","A","D","C","B"};
+		System.out.println("after sorting string array  ");
+		Arrays.sort(s);
+		for (String string : s) {
+			System.out.println(string);
+		}
+		
+		List<String> list = Arrays.asList(s);
+		//s[1]="E";
+		System.out.println("after converting into list  "+list);
+		// changes to list also reflect on array
+		// changes to array also reflect on list
+		s[1]="E";
+		list.set(2,"Y");
+		System.out.println("after converting into list  "+list);
+		for (String string : s) {
+			System.out.println(string);
+		}
+		
+	}
+
+}
