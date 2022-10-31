84a94ae311d63a057fd66b13ac0fe774b2c89cd9
bhawnachauhan07 <92302182+bhawnachauhan07@users.noreply.github.com>
2022-10-31 15:14:15 +0530

    i have updated the code

--- Arraysort.java
+++ Arraysort.java
@@ -29,6 +29,56 @@ public class Arraysort {
 		for (String string : s) {
 			System.out.println(string);
 		}
+		Hosteller h1 = new Hosteller();
+	
+	Scanner sc = new Scanner(System.in);
+	
+	System.out.println("Enter the details: ");
+	
+	System.out.println("Student Id");
+	
+	int a1 = sc.nextInt();
+	
+	System.out.println("Student Name");
+	
+	sc.nextLine();
+	
+	String b1=sc.nextLine();
+	
+	System.out.println("Department Id"); 
+	int c1 = sc.nextInt();
+	
+	System.out.println("Gender");
+	
+	sc.nextLine();
+	
+	String d1=sc.nextLine();
+	
+	System.out.println("Phone Number");
+	
+	String e1 = sc.nextLine();
+	
+	System.out.println("Hostel Name");
+	
+	String f1=sc.nextLine();
+	System.out.println("Room Number");
+	
+	int g1=sc.nextInt();
+	
+	h1.setStudentId(a1);
+	
+	h1.setName(b1);
+	
+	h1.setDepartmentId(c1);
+	
+	h1.setGender(d1);
+	
+	h1.setPhone(e1);
+	
+	h1.setHostelName(f1);
+	
+	h1.setRoomNumber(g1);
+	
 		
 		Arrays.sort(s,new Mycomparator());
 		System.out.println("after reverse sorting string array  ");
